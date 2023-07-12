bbd5ae02e5d519dea71e138087284e1c3956a593
Kevin Miranda <83476983+KevinMiranda26@users.noreply.github.com>
2023-07-12 12:09:09 -0500

    Delete files directory
    Delete uninstall.sh
    
    Delete module.prop
    
    Delete customize.sh
    
    Delete system directory
    
    Delete addon directory
    
    Delete META-INF/com/google/android directory
    
    Co-Authored-By: Kevin Miranda <83476983+KevinMiranda26@users.noreply.github.com>

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-rm -rf /data/resource-cache/*
-rm -rf /data/system/package_cache/*
-rm -rf /cache/*
-rm -rf /data/dalvik-cache/*
-pm uninstall com.kashi.pluginExt
-pm uninstall-system-updates miui.systemui.plugin
